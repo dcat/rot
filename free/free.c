31dc864d1488de67c8ffd20598c1e70d376fabf0
dcat <dcat@iotek.org>
2018-02-11 23:14:01 +0100

    add flags to free

--- free/free.c
+++ free/free.c
@@ -1,12 +1,35 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <err.h>
 
+
+void
+usage(char *name) {
+	fprintf(stderr, "usage: %s [-m | -k | -g]\n", name);
+	exit(0);
+}
 
 int
 main(int argc, char **argv) {
 	long pg, pgsiz, avphys;
 	int mod = 10;
+	char suf = ' ';
+
+	if (argc > 1) {
+		if (argv[1][0] == '-') {
+			switch (argv[1][1]) {
+			case 'm':
+				mod = 20;
+				suf = 'M';
+				break;
+			case 'g':
+				mod = 30;
+				suf = 'G';
+				break;
+			}
+		}
+	}
 
 	if ((pg = sysconf(_SC_PHYS_PAGES)) < 0)
 		err(1, "sysconf");
@@ -23,10 +46,10 @@ main(int argc, char **argv) {
 			11, "free"
 	);
 
-	printf("Mem:%*ld%*ld%*ld\n",
-			12, pg * pgsiz >> mod,
-			11, (pg - avphys) * pgsiz >> mod,
-			11, avphys * pgsiz >> mod
+	printf("Mem:%*ld%c%*ld%c%*ld%c\n",
+			12, pg * pgsiz >> mod, suf,
+			11, (pg - avphys) * pgsiz >> mod, suf,
+			11, avphys * pgsiz >> mod, suf
 	);
 
 	return 0;
